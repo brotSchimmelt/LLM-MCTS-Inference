335428f56aa1be8fd754e441c9e53b6ea85545fe
brotSchimmelt <tknebler+git@googlemail.com>
2025-01-11 18:00:12 +0100

    feat: add method to validate model settings

--- llm_mcts_inference/MonteCarloLLM.py
+++ llm_mcts_inference/MonteCarloLLM.py
@@ -1,7 +1,8 @@
 import os
 from dataclasses import dataclass
-from typing import Any, List
+from typing import Any, Dict, List
 
+from config import DEFAULT_SETTINGS, MODEL_SETTINGS
 from dotenv import load_dotenv
 
 
@@ -13,31 +14,53 @@ class MCTSResult:
 
 
 class MonteCarloLLM:
-    def __init__(self, model_name: str, endpoint: str = "", api_key: str = "") -> None:
+    def __init__(
+        self,
+        model_name: str,
+        endpoint: str = "",
+        api_key: str = "",
+        model_settings: Dict[str, Any] = {},
+    ) -> None:
         load_dotenv()
 
-        self.model_name = model_name
-        self.endpoint = endpoint if endpoint else "https://api.openai.com"
-        self.api_key = os.getenv("OPENAI_API_KEY") or os.getenv("API_KEY") or "EMPTY"
+        self.model_name = model_name if model_name else DEFAULT_SETTINGS["model"]
+        self.endpoint = endpoint if endpoint else DEFAULT_SETTINGS["base_url"]
+        self.api_key = api_key or os.getenv("OPENAI_API_KEY") or DEFAULT_SETTINGS["api_key"]
 
-        if not model_name:
-            raise ValueError("Please provide a model name.")
+        self.model_settings = self._validate_model_settings(model_settings)
 
-    def generate(self, prompt: str) -> MCTSResult:
-        """Generates a answer using Monte Carlo Tree Search with the given prompt.
+        self.model_settings["model_name"] = self.model_name
+        self.model_settings["base_url"] = self.endpoint
+        self.model_settings["api_key"] = self.api_key
+
+    def _validate_model_settings(self, model_settings: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Validates and updates the provided model settings against the default configuration.
 
         Args:
-            prompt (str): The input prompt or query.
+            model_settings (Dict[str, Any]): A dictionary of user-provided model settings
+                to validate and update. The keys in this dictionary should match the
+                expected keys defined in `MODEL_SETTINGS`.
 
         Returns:
-            MCTSResult: An object containing the generated answer, the complete MCTS tree,
-            and the valid path representing the sequence of decisions leading to the answer.
+            Dict[str, Any]: A validated dictionary of model settings. This includes all
+                the default values from `MODEL_SETTINGS`, updated with any valid keys
+                and values provided in the `model_settings` argument.
         """
+        valid_settings: Dict[str, Any] = MODEL_SETTINGS.copy()
+
+        for key, value in model_settings.items():
+            if key in valid_settings:
+                valid_settings[key] = value
+
+        return valid_settings
+
+    def generate(self, prompt: str) -> MCTSResult:
         raise NotImplementedError("Implement me!")
         return None
 
     def __str__(self) -> str:
-        return f"MonteCarloLLM(model_name={self.model_name}, endpoint={self.endpoint})"
+        return f"MonteCarloLLM(model_settings={self.model_settings}"
 
     def __repr__(self) -> str:
         return str(self)
